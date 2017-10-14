3ad2fab8e5179e395f288fc9e38fb3661838253e
Alex <amendment000@gmail.com>
2017-10-14 22:37:29 +0300

    commit

--- src/js/common.js
+++ src/js/common.js
@@ -11,9 +11,37 @@ $('.wrapp-bio').removeClass('bio-animate');
 		
 	});
 
+	$('#btn-area').hover(
+		function() {
+			$('.btn-move').addClass('btn-moving');
+			$(this).mousemove(function(e) {
+				var coordx = e.pageX - $('#btn-area').offset().left - $('.btn-move').outerWidth()/2;
+				var coordy = e.pageY - $('#btn-area').offset().top - $('.btn-move').outerHeight()*1.5;
+				$('.btn-move').css({
+					'left': coordx + 'px',
+					'top': coordy + 'px'
+				})
+			})
+		},
+		function() {
+			$('.btn-move').removeClass('btn-moving');
+			$('.btn-move').css({
+					'position': 'relative',
+					'left': 0,
+					'top': 0
+				})
+		})
+
 	var scene = document.getElementById('scene');
 	var parallax = new Parallax(scene);
+
+	var scenefooter = document.getElementById('scenefooter');
+	var parallax = new Parallax(scenefooter);
 	
+	var div = document.querySelectorAll('#works .row>div:first-child');
+	for (var i = 0; i < div.length; i = i + 2) {
+			 div[i].classList.add('order-2');
+	}
 	
 	$(document).scroll( function() {
 		$('#about').css({'background-positionY': -$(window).scrollTop()/5 + 'px'});
@@ -37,6 +65,13 @@ $('.wrapp-bio').removeClass('bio-animate');
 		autoStart: false,
 		speed: 50,
 		cursor: false
-	})
+	});
+
+	$( '.main-descr' ).typeIt({
+		autoStart: false,
+		speed: 50,
+		cursor: false
+	});
+	
 
 });
